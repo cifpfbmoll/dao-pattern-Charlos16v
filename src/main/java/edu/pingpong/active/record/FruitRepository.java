45c21853ba8218a7de40b494de4696cc4b9576ab
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-06-01 19:39:13 +0200

    feat(FruitRepository): added FruitRepository the repository layer of the dao pattern

---
+++ src/main/java/edu/pingpong/active/record/FruitRepository.java
@@ -0,0 +1,29 @@
+package edu.pingpong.active.record;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import io.quarkus.panache.common.Sort;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+public class FruitRepository implements PanacheRepositoryBase<Fruit, Integer> {
+
+    public List<Fruit> getAllDataSortedById() {
+        return this.listAll(Sort.by("id").ascending());
+    }
+
+    public Optional<Fruit> getFruitByName(String name) {
+        return this.find("name", name).firstResultOptional();
+    }
+
+    public void addFruit(Fruit fruit){
+        this.persist(fruit);
+    }
+
+    public void removeFruitByName(String name) {
+        Optional<Fruit> fruit = this.getFruitByName(name);
+        fruit.ifPresent(this::delete);
+    }
+}
