ab4c93a39cc4d7575cd4074c944aeadee338a51c
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-06-01 19:39:35 +0200

    feat(FruitRepository): added FruitResource the resource layer of the dao pattern

---
+++ src/main/java/edu/pingpong/active/record/FruitResource.java
@@ -0,0 +1,53 @@
+package edu.pingpong.active.record;
+
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Optional;
+
+@Path("/fruits")
+public class FruitResource {
+
+    @Inject
+    FruitService service;
+
+    public FruitResource() {
+    }
+
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response fruitsData() {
+        return Response.ok(service.getData(), MediaType.APPLICATION_JSON).build();
+    }
+
+    @POST
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addData(@Valid Fruit fruit) {
+        service.addFruit(fruit);
+        return Response.accepted(fruit).build();
+    }
+
+    @DELETE
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteData(@Valid Fruit fruit) {
+        service.removeFruit(fruit.getName());
+        return Response.accepted(fruit).build();
+    }
+
+    @GET
+    @Path("/{fruitname}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getData(@PathParam("fruitname") String fruitname) {
+        Optional<Fruit> fruit = service.getFruit(fruitname);
+        return fruit.isPresent() ? Response.ok(fruit).build() : Response.status(Response.Status.NOT_FOUND).entity("The fruit with name " + fruitname + " doesn't exist.").build();
+    }
+}
